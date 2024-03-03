1b112c1c71ffaf9609876f624f185998d205c00a
Lor1kb <149721177+Lor1kb@users.noreply.github.com>
2024-03-03 18:43:38 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const jsonServer = require("json-server");
+
+const jsonServerPort = 8095;
+
+const server = jsonServer.create();
+
+const middlewares = jsonServer.defaults();
+server.use(jsonServer.bodyParser);
+server.use(middlewares); //logger, cors etc.
+
+//const restaurantsRoutes = require(".routes/restaurants");
+//const menyRoutes = require(".routes/meny");
+
+//restaurantsRoutes(server);
+//menyRoutes(server);
+
+server.listen(jsonServerPort, () =>{
+    console.log(`JSON Server is runing on ${jsonServerPort}`);
+});
